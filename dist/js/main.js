c7ba4a43d9f989a41fe3a802f8ff18e15b4c246b
Vojta Oupicky <voj.oup@gmail.com>
2018-08-19 15:08:02 +0200

    Add menu animations

---
+++ dist/js/main.js
@@ -0,0 +1,40 @@
+// Select DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set the initial state for the menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  console.log('click');
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+
+    navItems.forEach(item => {
+      item.classList.add('show');
+    });
+
+    // Set the menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+
+    navItems.forEach(item => {
+      item.classList.remove('show');
+    });
+
+    // Set the menu state
+    showMenu = false;
+  }
+}
